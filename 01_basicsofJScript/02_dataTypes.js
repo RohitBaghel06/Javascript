bee31b436b61222e3a84263041191314a059538c
RohitBaghel06 <rohitb0619@gmail.com>
2024-07-20 14:55:33 +0530

    added dataTypes

---
+++ 01_basicsofJScript/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; //treat all JS code as newer version
+
+//alert(3+3)  we are using nodejs, not browser
+
+console.log(3 +3); //code readeability should be high
+console.log("Rohit"); 
+
+
+let name = "rohit" //it is string in double inverted coma
+let age = 21 //this is number
+let isLoggedIn = false
+let state;
+
+// number => 2^53
+// bigint = whenever your number is very big then it is used like in facebook, redit, etc
+// string => "" suggestion to prefer double inverted coma
+// boolean => whether true oe false
+// Null => it is a standalone value/datatype
+// undefined => value is not assign now
+// symbol = used to find uniqueness
+// object
+
+console.log(typeof null) //null is a object
+console.log(typeof undefined); //undefined
