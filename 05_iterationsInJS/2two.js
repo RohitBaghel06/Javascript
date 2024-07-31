82458c931ab9ab9b40ce41a45424ff69c5096f6b
RohitBaghel06 <rohitb0619@gmail.com>
2024-07-31 20:14:02 +0530

    while-loop done

---
+++ 05_iterationsInJS/2two.js
@@ -0,0 +1,24 @@
+// While loops
+
+let index = 0
+while (index <= 10) {
+    // console.log(`value of index is ${index}`);
+    index = index + 2
+}
+
+let myArray = ['batman', 'spiderman', 'superman']
+
+let arr = 0
+while (arr < myArray.length) {
+    // console.log(`Value is ${myArray[arr]}`);
+    arr++
+}
+
+// Do while loop
+
+let score = 11
+
+do {
+    console.log(`Score is ${score}`); // in this, work is done first condition is checked later
+    score ++
+} while (score <= 10);
